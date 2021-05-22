10b23e0f04e5713e07f1a6f75875ac7d2505192e
rennta <kenkyuu.renta@gmail.com>
2021-05-22 21:18:24 +0900

    メイン処理追加

--- Arduino_Sensor.ino
+++ Arduino_Sensor.ino
@@ -7,10 +7,23 @@
 // [ ライブラリ読み込み ]
 #include "Serial_Sensor_Test.h"
 
+// [ 初期実行関数 ]
 void setup(){
     
+    // 初期化処理 
+    Serial.begin(9600);
+    double data;
+    
+    // インスタンス作成
+    Sensor* test = new Serial_Sensor_Test(&Serial);
+
+    // データ取得/シリアル出力
+    test->GetData(&data);
+    Serial.print(data);
+
 }
 
+// [ ループ関数 ]
 void loop(){
 
 }
